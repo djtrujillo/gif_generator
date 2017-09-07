8d26a8cc9cc119b6404049a38bcea202893da2f0
djtrujillo <danny.j.trujillo@gmail.com>
2017-09-07 12:26:39 -0600

    First commit

---
+++ spec/features/users/guest_becomes_a_user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "guest becomes a user" do
+  context "guest creates a new user" do
+    it "sees and fills out new user form" do
+      visit '/'
+
+      click_on("Create New User")
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in "user[username]", with: "dt123"
+      fill_in "user[password]", with: "1234"
+
+      click_on "Create User"
+
+      expect(current_path).to eq(user_path(User.last))
+      expect(page).to have_content("Welcome to GifGenerator dt123")
+      expect(page).to have_link("Logout")
